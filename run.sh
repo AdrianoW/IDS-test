0e26e249fed47cd4bc53e477b65f10003f5ab4fb
ddrum001 <ddrum001@gmail.com>
2015-07-01 19:42:03 -0700

    changed run.sh to get rid of permission issues and changed directory names

--- run.sh
+++ run.sh
@@ -2,14 +2,7 @@
 
 # example of the run script for running the word count
 
-# first I'll load all my dependencies
-apt-get install python-pandas
-
-# next I'll make sure that all my programs (written in Python in this example) have the proper permissions
-chmod a+x my_word_count.py
-chmod a+x my_running_median.py
-
-# finally I'll execute my programs, with the input directory wc_input and output the files in the directory wc_output
+# I'll execute my programs, with the input directory wc_input and output the files in the directory wc_output
 python ./src/my_word_count.py ./wc_input ./wc_output/wc_result.txt
 python ./src/my_running_median.py ./wc_input ./wc_output/med_result.txt
 
