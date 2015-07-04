778df64e37fcf85cefa8fd419a724148981e7d3b
David E Drummond <ddrum001@users.noreply.github.com>
2015-07-04 09:40:43 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 
 # I'll execute my programs, with the input directory tweet_input and output the files in the directory tweet_output
 python ./src/words_tweeted.py ./tweet_input/tweets.txt ./tweet_output/ft1.txt
-python ./src/median_tweeted.py ./tweet_input/tweets.txt ./tweet_output/ft2.txt
+python ./src/median_unique.py ./tweet_input/tweets.txt ./tweet_output/ft2.txt
 
 
 
